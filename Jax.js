981762bb0a8b59e3fbc58dc39b58fee8b13153bf
David Freitag <dvdfreitag@gmail.com>
2013-10-19 18:58:05 -0400

    fixed math rendering

--- Jax.js
+++ Jax.js
@@ -12,7 +12,7 @@ if(window.MathJax === undefined)
 			   'availableFonts: ["STIX","TeX"], ' +
 			   'linebreaks: { automatic:true }, '+
 			   'EqnChunk: (MathJax.Hub.Browser.isMobile ? 10 : 50) }, ' +
-			   'tex2jax: { inlineMath: [ ["$", "$"], ["\\\\(","\\\\)"] ], displayMath: [ ["$$","$$"], ["\\[", "\\]"] ], processEscapes: true, ignoreClass: "tex2jax_ignore|dno" }, ' +
+			   'tex2jax: { inlineMath: [ ["$", "$"] ], displayMath: [ ["$$","$$"] ], processEscapes: true, ignoreClass: "tex2jax_ignore|dno|last-message" }, ' +
 			   'TeX: { extensions: ["mhchem.js", "enclose.js", "cancel.js"], noUndefined: { attributes: { mathcolor: "red", mathbackground: "#FFEEEE", mathsize: "90%25" } } }, ' +
 			   'messageStyle: "none"}); ' +
 			   'MathJax.Hub.Startup.onload();';
