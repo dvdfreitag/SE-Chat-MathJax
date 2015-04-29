e19139f5887f5408b6a3b7b70f54cf691966b063
David Freitag <dvdfreitag@gmail.com>
2015-04-29 14:31:24 -0400

    Update extension
    Update adds support for chat over https

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 var tabs = new Array();
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if(changeInfo.status == 'complete' && tab.url.match("http://chat.stackexchange.com/rooms/.*")) {
+	if(changeInfo.status == 'complete' && tab.url.match(".+://chat.stackexchange.com/rooms/.*")) {
 		tabs.push(tabId);
 		chrome.tabs.executeScript(tabId, {code:"var	s = document.createElement('script');" +
 											   "s.src = chrome.extension.getURL('Jax.js');" +
