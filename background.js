998e70f4298606615ebde5906b12e73bed07934a
John Rennie <johnhsrennie@gmail.com>
2023-04-08 13:09:26 +0100

    First attempt at port to manifest v3 - doesn't load

--- background.js
+++ background.js
@@ -3,8 +3,18 @@ var tabs = new Array();
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if(changeInfo.status == 'complete' && tab.url.match(".+://chat.stackexchange.com/rooms/.*")) {
 		tabs.push(tabId);
-		chrome.tabs.executeScript(tabId, {code:"var	s = document.createElement('script');" +
-											   "s.src = chrome.extension.getURL('Jax.js');" +
-											   "(document.head||document.documentElement).appendChild(s);"});
+
+		const functionToExecute = () => {
+			var	s = document.createElement('script');
+			s.src = chrome.runtime.getURL('Jax.js');
+			(document.head||document.documentElement).appendChild(s);
+		};
+
+		chrome.scripting.executeScript({
+			target: {tabId: tabId}, 
+			function: functionToExecute,
+			}
+		)
+		.then(() => console.log("injected script file"));
 	}
 });
